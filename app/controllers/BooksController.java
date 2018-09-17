ace00d5ed03c38df49de0f1a2a4f3ba677bfd683
KevinHenkes <henkeskevin0@gmail.com>
2018-09-17 17:22:39 +0200

    Route created, now creation of book model

---
+++ app/controllers/BooksController.java
@@ -0,0 +1,41 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class BooksController extends Controller {
+
+    // for all books
+    public Result index() {
+        return TODO;
+    }
+
+    // to create book
+    public Result create() {
+        return TODO;
+    }
+
+    // to save book
+    public Result save() {
+        return TODO;
+    }
+
+    public Result edit(Integer id) {
+        return TODO;
+    }
+
+    public Result update() {
+        return TODO;
+    }
+
+    public Result destroy(Integer id) {
+        return TODO;
+    }
+
+    // for book details
+    public Result show(Integer id) {
+        return TODO;
+    }
+
+
+}
