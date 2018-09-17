2ab792ea43ffc23aea31a47f3559ba7d30fd4208
KevinHenkes <henkeskevin0@gmail.com>
2018-09-17 18:49:31 +0200

    Create book feature implemented

--- app/controllers/BooksController.java
+++ app/controllers/BooksController.java
@@ -1,23 +1,44 @@
 package controllers;
 
+import models.Book;
+import play.data.Form;
+import play.data.FormFactory;
 import play.mvc.Controller;
 import play.mvc.Result;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import views.html.books.*;
+
+import javax.inject.Inject;
+
 public class BooksController extends Controller {
+
+    @Inject
+    FormFactory formFactory;
 
     // for all books
     public Result index() {
-        return TODO;
+        Set<Book> books = Book.allBooks();
+
+        return ok(index.render(books));
     }
 
     // to create book
     public Result create() {
-        return TODO;
+        Form<Book> bookForm = formFactory.form(Book.class);
+
+        return ok(create.render(bookForm));
     }
 
     // to save book
     public Result save() {
-        return TODO;
+        Form<Book> bookForm = formFactory.form(Book.class).bindFromRequest();
+        Book book = bookForm.get();
+        Book.add(book);
+        return redirect(routes.BooksController.index());
     }
 
     public Result edit(Integer id) {
